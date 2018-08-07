917b5b979892156ea258fe8c64f1c615a3c56a5d
shodan <elyz.2489@gmail.com>
2018-08-07 10:32:30 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:123456a@ds215502.mlab.com:15502/cafe';
+    urlDB = process.env.MONGO_URI;
 }
-
+//mongodb://cafe-user:123456a@ds215502.mlab.com:15502/cafe
 process.env.URLDB = urlDB;
